0ecf4208e3914c80e8a524003219571a43e888f4
Ze Diogo <up202003529@edu.fe.up.pt>
2022-04-24 19:40:27 +0100

    Added small animations for headerBar

--- feup_jobs/lib/Components/NavigationBar.dart
+++ feup_jobs/lib/Components/NavigationBar.dart
@@ -1,31 +1,34 @@
-// ignore_for_file: file_names
-
 import 'package:flutter/material.dart';
 
 PreferredSizeWidget headerBar() {
   return AppBar(
     backgroundColor: const Color.fromARGB(255, 169, 47, 26),
     leading: GestureDetector(
-      onTap: () {/* Write listener code here */},
-      child: const Icon(
-        Icons.menu, // add custom icons also
+      onTap: () {},
+      child: IconButton(
+        icon: const Icon(Icons.menu, size: 26.0),
+        onPressed: () {},
       ),
     ),
     actions: <Widget>[
       Padding(
-        padding: const EdgeInsets.only(right: 20.0),
-        child: GestureDetector(
-          onTap: () {},
-          child: const Icon(Icons.share, size: 26.0),
-        ),
-      ),
+          padding: const EdgeInsets.only(right: 20.0),
+          child: GestureDetector(
+            onTap: () {},
+            child: IconButton(
+              icon: const Icon(Icons.share, size: 26.0),
+              onPressed: () {},
+            ),
+          )),
       Padding(
-        padding: const EdgeInsets.only(right: 20.0),
-        child: GestureDetector(
-          onTap: () {},
-          child: const Icon(Icons.search, size: 26.0),
-        ),
-      ),
+          padding: const EdgeInsets.only(right: 20.0),
+          child: GestureDetector(
+            onTap: () {},
+            child: IconButton(
+              icon: const Icon(Icons.search, size: 26.0),
+              onPressed: () {},
+            ),
+          )),
     ],
   );
 }
