1509ea8c2be47ad6a4596f070ec2d6aae228562f
Ze Diogo <up202003529@edu.fe.up.pt>
2022-05-30 14:40:26 +0100

    Applications consult page almost complete

--- feup_jobs/lib/applicationsPage.dart
+++ feup_jobs/lib/applicationsPage.dart
@@ -1,9 +1,24 @@
 // ignore: file_names
+// ignore_for_file: unnecessary_const
+
+import 'package:feup_jobs/Apply_State_Page.dart';
 import 'package:feup_jobs/Components/NavigationBar.dart';
 import 'package:flutter/material.dart';
 
+class Application {
+  String title;
+  String company;
+  double indicatorValue;
+
+  Application({
+    required this.title,
+    required this.company,
+    required this.indicatorValue,
+  });
+}
+
 class ListPage extends StatefulWidget {
-  ListPage({Key? key, required this.title}) : super(key: key);
+  const ListPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -12,56 +27,114 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  late List applications = getApplications();
+
   @override
   Widget build(BuildContext context) {
+    ListTile makeListTile(Application application) => ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          leading: Container(
+            padding: EdgeInsets.only(right: 12.0),
+            decoration: const BoxDecoration(
+                border: const Border(
+                    right:
+                        const BorderSide(width: 1.0, color: Colors.white24))),
+            child: const Icon(Icons.autorenew, color: Colors.white),
+          ),
+          title: Text(
+            application.title,
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+          subtitle: Row(
+            children: <Widget>[
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    // tag: 'hero',
+                    child: LinearProgressIndicator(
+                        backgroundColor:
+                            const Color.fromRGBO(209, 224, 224, 0.2),
+                        value: application.indicatorValue,
+                        valueColor: const AlwaysStoppedAnimation(Colors.green)),
+                  )),
+              Expanded(
+                flex: 4,
+                child: Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Text(application.company,
+                        style: const TextStyle(color: Colors.white))),
+              )
+            ],
+          ),
+          trailing: const Icon(Icons.keyboard_arrow_right,
+              color: Colors.white, size: 30.0),
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => ApplyPageState()));
+          },
+        );
+
+    Card makeCard(Application lesson) => Card(
+          elevation: 8.0,
+          margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+          child: Container(
+            decoration:
+                const BoxDecoration(color: Color.fromARGB(255, 110, 47, 50)),
+            child: makeListTile(lesson),
+          ),
+        );
+
+    final makeBody = Container(
+      // decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, 1.0)),
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: applications.length,
+        itemBuilder: (BuildContext context, int index) {
+          return makeCard(applications[index]);
+        },
+      ),
+    );
+
+    final topAppBar = AppBar(
+      elevation: 0.1,
+      backgroundColor: const Color.fromARGB(255, 169, 47, 26),
+      title: Text(widget.title),
+      actions: <Widget>[],
+    );
+
     return Scaffold(
-      appBar: const CustomAppBar(),
+      appBar: topAppBar,
       body: makeBody,
     );
   }
 }
 
-final makeBody = Container(
-  child: ListView.builder(
-    scrollDirection: Axis.vertical,
-    shrinkWrap: true,
-    itemCount: 5,
-    itemBuilder: (BuildContext context, int index) {
-      return makeCard;
-    },
-  ),
-);
-
-final makeCard = Card(
-  elevation: 8.0,
-  margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-  child: Container(
-    decoration: const BoxDecoration(color: Color.fromARGB(200, 169, 47, 26)),
-    child: makeListTile,
-  ),
-);
-
-final makeListTile = ListTile(
-    contentPadding:
-        const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-    leading: Container(
-      padding: const EdgeInsets.only(right: 12.0),
-      decoration: const BoxDecoration(
-          border: Border(right: BorderSide(width: 1.0, color: Colors.white24))),
-      child: const Icon(Icons.autorenew, color: Colors.white),
+List getApplications() {
+  return [
+    Application(
+      title: "Junior Data Scientist",
+      company: "Google",
+      indicatorValue: 0.75,
     ),
-    title: const Text(
-      "Introduction to Driving",
-      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+    Application(
+      title: "Senior Backend Developer",
+      company: "Facebook",
+      indicatorValue: 0.25,
     ),
-    // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
-
-    subtitle: Row(
-      // ignore: prefer_const_literals_to_create_immutables
-      children: <Widget>[
-        const Icon(Icons.linear_scale, color: Colors.yellowAccent),
-        const Text(" Intermediate", style: TextStyle(color: Colors.white))
-      ],
+    Application(
+      title: "Frontend Developer Trainee",
+      company: "Primavera",
+      indicatorValue: 0.5,
     ),
-    trailing: const Icon(Icons.keyboard_arrow_right,
-        color: Colors.white, size: 30.0));
+    Application(
+      title: "Fullstack Developer",
+      company: "Amazon",
+      indicatorValue: 0.75,
+    ),
+  ];
+}
